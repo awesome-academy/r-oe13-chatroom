09d55cea9d1119ba2e8d2906e1c5c6211e2ee27f
Huong Nguyen MTA <huongnt0809@gmail.com>
2018-12-27 10:04:12 +0700

    Create Database

---
+++ app/models/relastionship.rb
@@ -0,0 +1,3 @@
+class Relastionship < ApplicationRecord
+  belongs_to :user
+end
---
+++ db/migrate/20181228090025_create_chatrooms.rb
@@ -0,0 +1,9 @@
+class CreateChatrooms < ActiveRecord::Migration[5.2]
+  def change
+    create_table :chatrooms do |t|
+      t.string :name
+      t.integer :owner_id
+      t.timestamps
+    end
+  end
+end
