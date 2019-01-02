ebf3e470a04339b562d619baf766ecd53b7cf70a
iamPhong <truongvanphong97@gmail.com>
2019-01-02 15:09:15 +0700

    4827 Create Database

---
+++ db/migrate/20181228090631_create_relastionships.rb
@@ -0,0 +1,9 @@
+class CreateRelastionships < ActiveRecord::Migration[5.2]
+  def change
+    create_table :relastionships do |t|
+      t.references :user, foreign_key: true
+      t.integer :friend_id
+      t.timestamps
+    end
+  end
+end
