c05ac8541717beb47ebef16325bb123d2af4144b
iamanhquan <anhquanhh4@gmail.com>
2019-01-03 13:18:42 +0700

    4837 Chat Basic

--- app/controllers/chatroom_users_controller.rb
+++ app/controllers/chatroom_users_controller.rb
@@ -1,9 +1,18 @@
 class ChatroomUsersController < ApplicationController
 
   before_action :set_chatroom
-  def create; end
+  def create
+    @chatroom_user = @chatroom.chatroom_users.by_user(current_user.id).first_or_create
+    redirect_to @chatroom
+  end
 
-  def destroy; end
+  def destroy
+    respond_to do |format|
+      @chatroom_user = @chatroom.chatroom_users.by_user(current_user.id).destroy_all
+      format.js {render layout: false}
+      format.html {redirect_to chatrooms_path}
+    end
+  end
 
   private
   def set_chatroom
